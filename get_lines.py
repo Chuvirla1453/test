b7fe822fb2fc3e6341762c859bd050a695926579
Chuvirla1453 <artemis.arteyom.ivanov@gmail.com>
2022-02-11 19:20:37 +0300

    Пейзажи Марса

--- get_lines.py
+++ get_lines.py
@@ -4,76 +4,39 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def start_page():
-    return 'Миссия Колонизация Марса'
-
-
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion_image')
 def prom_image():
-    a = ['Человечество вырастает из детства.',
-         'Человечеству мала одна планета.',
-         'Мы сделаем обитаемыми безжизненные пока планеты.',
-         'И начнем с Марса!',
-         'Присоединяйся!'
-         ]
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                     <link rel="stylesheet" 
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                     crossorigin="anonymous">
-                    <title>Привет, Марс!</title>
+                    <title>Карусель</title>
                   </head>
                   <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}">
-                    <div class="alert alert-dark" role="alert">
-                      Человечество вырастает из детства.</br>
+                    <div id="1" class="carousel slide" data-bs-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="3000">
+                          <img src="{url_for('static', filename='img/1.jpg')}" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                          <img src="{url_for('static', filename='img/2.jpg')}" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                          <img src="{url_for('static', filename='img/3.jpg')}" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                          <img src="{url_for('static', filename='img/4.jpg')}" class="d-block w-100" alt="...">
+                        </div>
+                      </div>
                     </div>
-                    <div class="alert alert-success" role="alert">
-                      Человечеству мала одна планета.</br>
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.</br>
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      И начнем с Марса!</br>
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Присоединяйся!</br>
-                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                      crossorigin="anonymous"></script>
                   </body>
                 </html>"""
-
-
-@app.route('/bootstrap_sample')
-def bootstrap():
-    return '''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1>Привет, Яндекс!</h1>
-                    <div class="alert alert-primary" role="alert">
-                      А мы тут компонентами Bootstrap балуемся
-                    </div>
-                  </body>
-                </html>'''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
