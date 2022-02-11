219c1b6497d33599b59cc71651e97feac10b809c
Chuvirla1453 <artemis.arteyom.ivanov@gmail.com>
2022-02-11 18:30:06 +0300

    Реклама с картинкой

--- get_lines.py
+++ get_lines.py
@@ -13,32 +13,67 @@ def index():
     return "И на Марсе будут яблони цвести!"
 
 
-@app.route('/promotion')
-def promotion():
+@app.route('/promotion_image')
+def prom_image():
     a = ['Человечество вырастает из детства.',
          'Человечеству мала одна планета.',
          'Мы сделаем обитаемыми безжизненные пока планеты.',
          'И начнем с Марса!',
          'Присоединяйся!'
          ]
-    return '</br>'.join(a)
-
-
-@app.route('/image_mars')
-def image():
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
                     <title>Привет, Марс!</title>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/mars.png')}">
-                    <p>Это колбаса на самом деле</p>
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства.</br>
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.</br>
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.</br>
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!</br>
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!</br>
+                    </div>
                   </body>
                 </html>"""
 
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
