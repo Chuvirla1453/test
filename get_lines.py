2a37cf57ad310e72181a8619e51e5ca6443ad94c
Chuvirla1453 <artemis.arteyom.ivanov@gmail.com>
2022-02-20 20:23:09 +0300

    Готовимся к миссии

--- get_lines.py
+++ get_lines.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
 @app.route('/')
 def start_page():
-    print()
-    return render_template("base.html", image_url=url_for('static', filename='img/mars.jpg'), title="База")
+    return '<a href="http://127.0.0.1:8080/Заготовка"> Заготовка </a>'
+
+@app.route('/<title>')
+def base_page(title):
+    return render_template("base.html", image_url=url_for('static', filename='img/mars.jpg'), title=title)
 
 
 if __name__ == '__main__':
