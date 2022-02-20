6337f392396f1bbc3d141cf0980287e209010ee7
Chuvirla1453 <artemis.arteyom.ivanov@gmail.com>
2022-02-20 21:15:11 +0300

    Тренировки в полёте

--- get_lines.py
+++ get_lines.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template, url_for
-
 app = Flask(__name__)
+
+
 @app.route('/')
 def start_page():
-    return '<a href="http://127.0.0.1:8080/Заготовка"> Заготовка </a>'
+    return '<a href="http://127.0.0.1:8080/training/инженер"> Заготовка </a>'
+
+
+@app.route('/training/<prof>')
+def prof_page(prof):
+    params = {
+        "mars_image_url": url_for('static', filename='img/mars.jpg'),
+        "prof": prof
+    }
+    if 'инженер' in prof or "сторитель" in prof:
+        params["training"] = "Инженерные тренажёры"
+        params["plan_of_training_image_url"] = url_for('static', filename='img/ES.jpg')
+    else:
+        params["training"] = "Научные симуляторы"
+        params["plan_of_training_image_url"] = url_for('static', filename='img/SS.jpg')
+    return render_template("prof.html", **params)
+
 
 @app.route('/<title>')
 def base_page(title):
@@ -12,3 +29,4 @@ def base_page(title):
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
+    print(url_for('static', filename='img/mars.jpg'))
