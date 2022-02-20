50455649c190c44ad77e0ba5b7f048f0de3de396
Chuvirla1453 <artemis.arteyom.ivanov@gmail.com>
2022-02-20 20:18:30 +0300

    Готовимся к миссии

--- get_lines.py
+++ get_lines.py
@@ -1,42 +1,12 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def prom_image():
-    return f"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Карусель</title>
-                  </head>
-                  <body>
-                    <div id="1" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-inner">
-                        <div class="carousel-item active" data-bs-interval="3000">
-                          <img src="{url_for('static', filename='img/1.jpg')}" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="3000">
-                          <img src="{url_for('static', filename='img/2.jpg')}" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="3000">
-                          <img src="{url_for('static', filename='img/3.jpg')}" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="3000">
-                          <img src="{url_for('static', filename='img/4.jpg')}" class="d-block w-100" alt="...">
-                        </div>
-                      </div>
-                    </div>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                      crossorigin="anonymous"></script>
-                  </body>
-                </html>"""
+def start_page():
+    print()
+    return render_template("base.html", image_url=url_for('static', filename='img/mars.jpg'), title="База")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
